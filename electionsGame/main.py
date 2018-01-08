7dfda91404c04f99c6661b4a69b6c2c7ed3ec299
norik16 <ron.norik@gmail.com>
2018-01-08 04:51:56 +0100

    Fixed long run average money gain

--- electionsGame/main.py
+++ electionsGame/main.py
@@ -11,12 +11,12 @@ change = 1 / 2
 class Party:
     """Generic political party class"""
 
-    def __init__(self, name: str, color: str, selfesteem: int, plunder: float,
-                 money: int = 0, promises: int = 100, karma: int = 75,
-                 start: int = 0):
+    def __init__(self, name: str, color: str, selfesteem: int = 10,
+                 plunder: float = 0.1, money: int = 0, promises: int = 100,
+                 karma: int = 75, start: int = 0):
         self.name = name
         self.color = color
-        self.selfesteam = selfesteem
+        self.selfesteem = selfesteem
         self.plunder = plunder
         self.promises = promises
         self.karma = karma
@@ -28,17 +28,17 @@ class Party:
         self.last_parliament = 1  # last percentage in parliament
         self.rounds_nip = 0  # how many rounds not in parliament
         self.t_rounds_ip = 1  # total num. of rounds in parliament
+        self.last_votes = 0
 
     def rule(self, parliament):
         """Evaluate ruling of the party based on promises a plundering"""
 
-        print(self.name, " : Rulez!")
         self.rounds_nip = 0
         self.t_rounds_ip += 1
 
-        # After getting into the parliament everything promised by selfesteam
+        # After getting into the parliament everything promised by selfesteem
         # becomes real promise
-        self.promises = self.selfesteam
+        self.promises = self.selfesteem
 
         if self.rounds >= self.start:
             self.money += self.plunder * parliament * 2
@@ -70,16 +70,10 @@ class Party:
         self.rounds += 1
 
         if self.rounds >= self.start:
-            print(self.get_name(), " >> karma", self.karma,
-                  " >> done_promises", self.done_promises, "  >> plunder ",
-                  self.plunder)
+            self.last_votes = self.selfesteem * self.karma \
+                              * math.exp(- (self.last_parliament / 60) ** 3)
 
-            # Another method:
-            # return (self.done_promises + self.selfesteam) * self.karma * math.exp(
-            #     -(self.last_parliament / 60) ** 3)
-            return self.selfesteam * self.karma \
-                   * math.exp(- (self.last_parliament / 60) ** 3)
-            # / (1 + self._last_parliament/2)
+            return self.last_votes
 
         return 0
 
@@ -88,7 +82,6 @@ class Party:
             What should the party do while not being in parliament
         """
 
-        print(self.name + " : Not in parliament")
         if self.rounds < self.start:
             return
         self.karma += 1  # Slowly regain karma
@@ -106,6 +99,14 @@ class Party:
     def get_color(self):
         return self.color
 
+    def get_info(self):
+        print(self.name,
+              '|   \tKarma', round(self.karma, 1),
+              '\tselfesteem', round(self.selfesteem, 1),
+              '\tPlunder', round(self.plunder, 1),
+              '  \tLast Partliament', round(self.last_parliament, 1),
+              '\tLast votes', round(self.last_votes, 1))
+
 
 class Smart(Party):
     """
@@ -114,18 +115,22 @@ class Smart(Party):
     """
 
     def rule(self, parliament):
-        super().rule(parliament)
 
         self.plunder += 0.1
+        if self.plunder > 1:
+            self.plunder = 1
+
+        super().rule(parliament)
 
     def not_in_parliament(self):
-        super().not_in_parliament()
 
         self.plunder -= 0.1
         if self.plunder < 0:
             self.plunder = 0
         # if self.rounds_nip > random.randint(3, 5):
         #     self.karma = 1
+
+        super().not_in_parliament()
 
 
 class SmarterBinary(Party):
@@ -134,15 +139,16 @@ class SmarterBinary(Party):
         and starts plundering only while above it
     """
 
-    def __init__(self, name: str, color: str, selfesteem: int, plunder: float,
-                 money: int = 0, promises: int = 100, karma: int = 75,
-                 start: int = 0, goal: int = 50):
+    def __init__(self, name: str, color: str, selfesteem: int = 10,
+                 plunder: float = 0.1, money: int = 0, promises: int = 100,
+                 karma: int = 75, start: int = 0, goal: int = 50):
 
         super().__init__(name, color, selfesteem, plunder, money, promises,
                          karma, start)
 
         self.state = 0  # 0 = growing, 1 = eating
         self.goal = goal
+        self.selfesteem = self.goal
 
     def rule(self, parliament):
         if parliament < self.goal:
@@ -166,15 +172,16 @@ class SmarterLinear(Party):
         and is plundering in proportion to % needed to achieving it
     """
 
-    def __init__(self, name: str, color: str, selfesteem: int, plunder: float,
-                 money: int = 0, promises: int = 100, karma: int = 75,
-                 start: int = 0, goal: int = 50):
+    def __init__(self, name: str, color: str, selfesteem: int = 10,
+                 plunder: float = 0.1, money: int = 0, promises: int = 100,
+                 karma: int = 75, start: int = 0, goal: int = 50):
 
         super().__init__(name, color, selfesteem, plunder, money, promises,
                          karma, start)
 
         self.state = 0  # 0 = growing, 1 = eating
         self.goal = goal
+        self.selfesteem = self.goal
 
     def rule(self, parliament):
         self.plunder = (parliament - self.goal) / 25 * change
@@ -186,6 +193,32 @@ class SmarterLinear(Party):
         super().rule(parliament)
 
 
+class SmarterSelfesteem(SmarterLinear):
+    """
+        This party sets selfesteem a bit higher if it is successful
+    """
+
+    def rule(self, parliament):
+        self.selfesteem += 1
+        self.selfesteem = min(self.selfesteem, 100)
+
+        super().rule(parliament)
+
+    def not_in_parliament(self):
+        self.selfesteem -= 5
+        self.selfesteem = max(self.selfesteem, self.goal)
+
+        super().not_in_parliament()
+
+# Prototype
+# class RandParty(Party):
+#     def get_votes(self):
+#         self.selfesteem = random.randint(0, 100)
+#         self.plunder = random.randint(0, 100) / 100
+#
+#         super().get_votes()
+
+
 def run_the_state(parties, number_of_elections, results):
     votes = {}
 
@@ -194,15 +227,19 @@ def run_the_state(parties, number_of_elections, results):
         votes[party.get_name()] = 0
 
     for it in range(number_of_elections):
-        print("Iteration " + str(it))
-        print("------------------------")
+        if it % (number_of_elections/10) == -1:
+            print('\nIteration ' + str(it))
+            for party in parties:
+                # if party.get_name() == 'ODS':
+                party.get_info()
+
+            print('------------------------')
         preTotalVotes = 0
         totalVotes = 0
 
         # Get votes for each party
         for party in parties:
             votes[party.get_name()] = party.get_votes()
-            # print(party.get_name() + " got " + str(votes[party.get_name()]) + " out of " + str(totalVotes))
 
         # Count total number of votes
         for party in parties:
@@ -217,10 +254,6 @@ def run_the_state(parties, number_of_elections, results):
 
         # Decide if party should be in parliament
         for party in parties:
-            print(party.get_name()
-                  + ": " + str(votes[party.get_name()] / preTotalVotes * 100)
-                  + "   |   " + str(votes[party.get_name()]))
-
             if votes[party.get_name()] / preTotalVotes * 100 <= 5:
                 votes[party.get_name()] = 0
                 party.not_in_parliament()
@@ -245,25 +278,22 @@ def run_the_state(parties, number_of_elections, results):
 
     # Get avg money earned per election
     for party in parties:
-        results[party.get_name()].append(party.get_money()/number_of_elections)
+        results[party.get_name()].append(
+            party.get_money() / number_of_elections)
 
     return results
 
 
-def output(number_of_elections, parties):
+def output(lengths, parties):
     """
         Money per election for different time periods
 
-        number_of_elections : int
+        lengths : int -- different time periods
         parties : {name: money}
     """
 
     p = {}
     results = {}
-
-    lengths = [x for x in range(2, 100)]  # timeperiods to test
-    # lengths = [2, 4]  # timeperiods to test
-
 
     for party in parties:
         results[party.get_name()] = []
@@ -271,18 +301,6 @@ def output(number_of_elections, parties):
     for length in lengths:
         c_parties = copy.deepcopy(parties)
         results = run_the_state(c_parties, length, results)
-
-        # for party in parties:
-        #     print()
-        #     print()
-        #     print("Money:")
-        #     print("------------------------")
-        #     for party in parties:
-        #         print(party.get_name() + ": " + str(
-        #             party.get_money()) + ' MKč while being ' + str(
-        #             party.t_rounds_ip) +
-        #               ' times elected -> efficiency  ' + str(
-        #             party.get_money() / party.t_rounds_ip))
 
     for party in parties:  # y = money per year, x = number of years
         plt.plot([i for i in lengths],
@@ -292,60 +310,67 @@ def output(number_of_elections, parties):
     plt.show()
 
 
-# TODO: put into fcions
-
 
 # name, color, selfesteem, plunder, money, promises, karma, start
 
 # big fight, cycling after hudereds
 parties1 = [
-    Smart("ODS", 'b', 5, 0.2, 0, 60, 50),
-    Smart("ČSSD", 'r', 42, 0.3854, 0, 60, 40),
-    Smart("KDU-ČSL", 'y', 5, 0.2, 0, 50, 20),
-    Smart("ANO", 'black', 30, 0.50, 0, 100, 70, 60),
-    Smart("Ja", 'grey', 50, 0, 0, 10, 50, 80),
-    SmarterBinary("Nein", 'pink', 50, 0.5, 0, 10, 50, 90), ]
+    Smart('ODS', 'b', 5, 0.2, 0, 60, 50),
+    Smart('ČSSD', 'r', 42, 0.3854, 0, 60, 40),
+    Smart('KDU-ČSL', 'y', 5, 0.2, 0, 50, 20),
+    Smart('ANO', 'black', 30, 0.50, 0, 100, 70, 60),
+    Smart('Ja', 'grey', 50, 0, 0, 10, 50, 80),
+    SmarterBinary('Nein', 'pink', 50, 0.5, 0, 10, 50, 90), ]
 
 # two blocked smarter strategies
 parties2 = [
-    Smart("ODS", 'b', 5, 0.2, 0, 60, 50),
-    SmarterBinary("ČSSD", 'r', 5, 0.2, 0, 60, 50, 0, 75),
-    SmarterLinear("KDU-ČSL", 'y', 5, 0.2, 0, 60, 50, 0, 75), ]
+    Smart('ODS', 'b', 5, 0.2, 0, 60, 50),
+    SmarterBinary('ČSSD', 'r', 5, 0.2, 0, 60, 50, 0, 75),
+    SmarterLinear('KDU-ČSL', 'y', 5, 0.2, 0, 60, 50, 0, 75), ]
 
 # two smarter strategies outsmarting the smart one
 parties3 = [
-    Smart("ODS", 'b', 5, 0.2, 0, 60, 50),
-    SmarterBinary("ČSSD", 'r', 5, 0.2, 0, 60, 50, 0, 30),
-    SmarterLinear("KDU-ČSL", 'y', 5, 0.2, 0, 60, 50, 0, 30), ]
+    Smart('ODS', 'b', 5, 0.2, 0, 60, 50),
+    SmarterBinary('ČSSD', 'r', 5, 0.2, 0, 60, 50, 0, 30),
+    SmarterLinear('KDU-ČSL', 'y', 5, 0.2, 0, 60, 50, 0, 30), ]
 
-p = []
+# test how many patrties can coexist before noone gets above 5 %
+parties4 = []
+for i in range(1, 5):
+    parties4.append(Smart('ODS' + str(i), 'b', 5, i / 100, 0, 60, 50))
+    parties4.append(
+        SmarterBinary('ČSSD' + str(i), 'r', 5, 0.5, 0, 60, 50, 0, i))
+    parties4.append(
+        SmarterLinear('KDU-ČSL' + str(i), 'y', 5, 0.5, 0, 60, 50, 0, i))
 
-for i in range(1, 100):
-    parties = []
-    parties.append(Smart("ODS", 'b', 5, i / 100, 0, 60, 50))
-    parties.append(SmarterBinary("ČSSD", 'r', 5, 0.5, 0, 60, 50, 0, i))
-    parties.append(SmarterLinear("KDU-ČSL", 'y', 5, 0.5, 0, 60, 50, 0, i))
+# interesting setup with the most sophisticated party joining later
+parties5 = [
+    Smart('ODS', 'b'),
+    SmarterBinary('Piráti', 'black', goal=20),
+    SmarterLinear('ČSSD', 'orange', goal=20),
+    SmarterSelfesteem('Ne', 'r', start=30, goal=20)
+]
 
-#     p.append(parties)
-#
-# for parties in p:
-#     for party in parties:
-#         if party.goal:
-#             party.name += ' ' + party.goal
-#         else:
-#             party.name += ' ' + party.plunder
-#
-# for parties in p:
-#     mn = []
-#     for i in range(5, 200):
-#         mn.append(i, run_the_state(parties, i))
+# Three almost identical strategies, each one has to be dominating
+# to start plundering
+parties6 = [
+    SmarterBinary('Piráti', 'black',goal=61),
+    SmarterBinary('ČSSD', 'orange', start=20,goal=63),
+    SmarterBinary('Ne', 'r', start=10,goal=60)
+]
 
-# Party("ANO", 'black', 30, 0.50, 0, 100, 100, 60),
-# Party("Ja", 'grey', 2, 0.50, 0, 100, 100, 60),
+# Showcase of all 4 strategies
+parties0 = [
+    Smart('ODS', 'b'),
+    SmarterBinary('Piráti', 'black'),
+    SmarterLinear('ČSSD', 'orange'),
+    SmarterSelfesteem('Ne', 'r', start=50)
+]
 
-number_of_years = 200
 
-output(100, parties1)
 
+lengths = [x for x in range(2, 300)]  # timeperiods to test
+
+output(lengths, parties0)
 
 # runTheState(parties1, number_of_years)
